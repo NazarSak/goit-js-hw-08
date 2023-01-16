18a5aeb1a9da8443940a2f382b12f04cc7d89230
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-01-17 01:16:18 +0200

    4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,11 @@
 import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+import lodash from "lodash.throttle"
 
 const iframe = document.querySelector('iframe');
 
+// 
 
 const player = new Player(iframe);
 
@@ -12,23 +16,25 @@ player.on('play', function() {
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
- 
+
+//  
    
 const onPlay = function(data) {
-
+ localStorage.setItem("videoplayer_current_time",data.seconds);
 };
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay,1000));
+// 
 
-const time = +localStorage.getItem(VIDEO_CURRENT_TIME);
+const currentTime = Number(localStorage.getItem("videoplayer_current_time"))
 
-player.setCurrentTime(time).then(function(seconds) {
+player.setCurrentTime(currentTime).then(function(seconds) {
     // seconds = the actual time that the player seeked to
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
             // the time was less than 0 or greater than the video’s duration
-            break;
+             break;
 
         default:
             // some other error occurred
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import lodash from "lodash.throttle"
+
+
+const form = document.querySelector(".feedback-form");
+const input = document.querySelector(".email");
+const message = document.querySelector(".message")
+
+form.addEventListener("input",inputForm)
+
+form.addEventListener("submit",submitForm)
+
+// 
+
+function inputForm (evt) {
+formData[evt.target.name] = evt.target.value.trim();
+localStorage.setItem("feedback-form-state",JSON.stringify(FormData));    
+}
+
+
+function submitForm (evt) {
+    evt.preventDefault()
+
+  const parseData = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+  evt.currentTarget.reset()
+
+  localStorage.removeItem("feedback-form-state");
+  formData = {};
+}
+
+function reloadPAge () {
+   const savedlocal =  localStorage.getItem("feedback-form-state")
+   if (savedValues) {
+    formData = JSON.parse(savedlocal);
+    console.log(formData);
+    input.value = formData.email;
+    textarea.value = formData.message;
+}
+
+}
 
